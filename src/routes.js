7d055dcf4e33737575d432d97040db7cb32aa394
Jesus <michel.js66@gmail.com>
2020-04-12 19:13:06 -0300

    adicionado UserController

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.post('/users', function(req, res) {
+  UserController.store(req, res);
+});
 
 export default routes;
